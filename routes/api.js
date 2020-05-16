35db972b08a3acce175412a80bc5b44883994870
Glitch (issue-tracker-hm) <none>
2020-05-16 10:58:41 +0000

    routes sketched in

--- routes/api.js
+++ routes/api.js
@@ -8,34 +8,127 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-var ObjectId = require('mongodb').ObjectID;
+const expect = require('chai').expect;
+const MongoClient = require('mongodb');
+const ObjectId = require('mongodb').ObjectID;
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const CONNECTION_STRING = process.env.MONGO_URI;
+
 
 module.exports = function (app) {
+  
 
   app.route('/api/issues/:project')
   
-    .get(function (req, res){
-      var project = req.params.project;
-      
+    .get(function (req, res, next){
+      const project = req.params.project;
+    
+      MongoClient.connect(CONNECTION_STRING, (err, client) => {
+        const db = client.db('issueTracker');
+        if(err) {
+        console.log('Database err: ' + err);
+        } else {
+        console.log('Successful database connection');
+        }
+      }) 
     })
     
-    .post(function (req, res){
-      var project = req.params.project;
-      
+    .post((req, res, next) => {
+      const project = req.params.project;
+      const issue = {
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || "",
+        status_text: req.body.status_text || "",
+        created_on: new Date(Date.now),
+        updated_on: new Date(Date.now),
+        open: true
+      }
+      if(!issue.issue_title) {
+        const err = new Error("missing issue_title");
+        err.status = 400;
+        return next(err);
+      }
+      if(!issue.issue_text) {
+        const err = new Error("missing issue_text");
+        err.status = 400;
+        return next(err);
+      }
+      if(!issue.created_by) {
+      const err = new Error("missing created_by");
+      err.status = 400;
+      return next(err);
+      }
+      MongoClient.connect(CONNECTION_STRING, (err, client) => {
+        const db = client.db('issueTracker');
+        if(err) {
+        console.log('Database err: ' + err);
+        } else {
+        console.log('Successful database connection');
+        db.collection('issues').insertOne(issue, (err, doc) => {
+          res.json(issue);
+        })
+        }
+      }) 
     })
     
-    .put(function (req, res){
-      var project = req.params.project;
-      
+    .put(function (req, res, next){
+      const project = req.params.project;
+      const issueId = req.body._id;
+      const issue = req.body;
+      delete issue._id;
+      Object.keys(issue).map((key) => {if(issue.key === null) delete issue.key});
+      if(Object.keys(issue).length === 0) {
+        return res.send('no updated field sent');
+      } else {
+        issue.updated_on = new Date(Date.now);
+        MongoClient.connect(CONNECTION_STRING, (err, client) => {
+          const db = client.db('issueTracker');
+          if(err) {
+            console.log('Database err: ' + err);
+          } else {
+            console.log('Successful database connection');
+            db.collection('issues').findAndModify({_id: issueId},{$set: issue}, {new: true}, (err, doc) => {
+              if (!err) {
+                res.send('successfully updated')
+              } else {
+                const error = new Error(`could not update ${issueId} ${err}`);
+                error.status = 400;
+                next(error);
+              }
+            })
+          }
+        })
+      }
     })
     
-    .delete(function (req, res){
-      var project = req.params.project;
+    .delete(function (req, res, next){
+      const project = req.params.project;
+      const issueId = req.body._id;
+      if(!issueId) {
+        const err = new Error('_id error');
+        return next(err);
+      } else {
+        MongoClient.connect(CONNECTION_STRING, (err, client) => {
+          const db = client.db('issueTracker');
+          if(err) {
+            console.log('Database err: ' + err);
+          } else {
+            console.log('Successful database connection');
+            db.collection('issues').findAndRemove({_id: issueId}, (err, doc) => {
+              if(err) {
+                const error = new Error(`could not delete ${issueId} ${err}`);
+                return next(error);
+              } else {
+                res.send(`deleted ${issueId}`);
+              }
+            })
+          }
+        })
+      }
+    
       
     });
-    
+  
 };
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const expect      = require('chai').expect;
+const cors        = require('cors');
+const helmet      = require('helmet');
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
 
 var app = express();
 
@@ -15,7 +16,7 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 
-
+app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -39,10 +40,19 @@ fccTestingRoutes(app);
 apiRoutes(app);  
     
 //404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.use((err, req, res, next) => {
+  let errCode, errMessage;
+  if(err.errors) {
+    //mongoose validation error
+    errCode = 400;
+    const keys = Object.keys(err.errors);
+    errMessage = err.errors[keys[0]].message;
+  } else {
+    //generic or custom error
+    errCode = err.status || 404;
+    errMessage = err.message || "Not Found";
+  }
+  res.status(errCode).json({error: errMessage});
 });
 
 //Start our server and tests!
